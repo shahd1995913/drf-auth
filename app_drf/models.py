9d1ed8c7754457b560b04151aac48b1374fc0eed
shahd1995913 <shahdalkhatib95@gmail.com>
2021-11-23 23:52:12 +0400

    update

---
+++ app_drf/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+# Create your models here.
+
+
+
+
+class user(AbstractUser):
+    pass
+    # add additional fields in here
+
+    def __str__(self):
+        return self.username
